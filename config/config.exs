008508c0cdb4b4e253a8c1bbd7a18125c1b65967
Rajesh Rajagopalan <rrajesh1979@gmail.com>
2022-01-19 16:32:36 -0500

    :rocket: GitHub Workflow to build

---
+++ config/config.exs
@@ -0,0 +1,6 @@
+import Config
+
+# # Our Logger general configuration
+config :logger,
+  backends: [:console],
+  level: :info
--- lib/ref_elixir_basic_pipeline.ex
+++ lib/ref_elixir_basic_pipeline.ex
@@ -1,18 +1,49 @@
-defmodule RefElixirBasicPipeline do
+defmodule RefElixir.BasicPipeline do
   @moduledoc """
-  Documentation for `RefElixirBasicPipeline`.
+  Basic data processing pipeline.
+  Eagerly loads data from the source files.
+  Uses basic Elixir pipeline to process data.
+
+  synopsis:
+    Process credit requests provided as a feed file.
+  usage:
+    $ ./ref_elixir_basic_pipeline {options} arg1 arg2 ...
+  options:
+    --file        Feed file path or directory (required)
   """
 
-  @doc """
-  Hello world.
+  require Logger
 
-  ## Examples
+  def main(args \\ []) do
+    args
+    |> parse_args()
+    |> response()
+    |> process()
+  end
 
-      iex> RefElixirBasicPipeline.hello()
-      :world
+  defp parse_args(args) do
+    {opts, file_path, _} =
+      args
+      |> OptionParser.parse(switches: [file: :boolean])
 
-  """
-  def hello do
-    :world
+    {opts, List.to_string(file_path)}
+  end
+
+  defp response({opts, file_path}) do
+    case opts[:file] do
+      true ->
+        Logger.info("File path: #{inspect(file_path)}")
+
+      _ ->
+        Logger.error("
+        File path is not specified
+        usage:
+          ./ref_elixir_basic_pipeline {options} arg1 arg2 ...
+        ")
+    end
+  end
+
+  def process(file_path) do
+    File.read!(file_path)
   end
 end
